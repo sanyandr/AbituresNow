f34a90963d2e8178cac6b1e6daea58d3de2a4639
AlexanderBoot <alexenderboot@gmail.com>
2023-05-17 07:57:32 +0300

    05.17.2023 got hover info

--- src/Modules.py
+++ src/Modules.py
@@ -7,52 +7,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+import time 
+import chromedriver_autoinstaller
+import sys
+chromedriver_autoinstaller.install()
 
-url = 'https://priem.volsu.ru/rating'
-
-
-from selenium.webdriver.chrome.options import Options
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_argument("window-size=1200x600")
 options.add_argument('ignore-certificate-errors')
 options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) 
-driver.get(url)
-
-
-
-# # select first dropdown
-# # select = Select(driver.find_element(by=By.ID, value='level'))
-# # select.select_by_visible_text('Бакалавриат')
-
-# driver.execute_script("$('#level').val('  Бакалавриат').trigger('change')")
-# driver.execute_script("$('#oop').select2('open');")
-
-# driver.implicitly_wait(300)
-
-# select = Select(driver.find_element(by=By.ID, value='oop'))
-# select.select_by_value('000002948')
-
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-
-# wait = WebDriverWait(20)
-# pri = wait.until(EC.element_to_be_clickable(By.XPATH, "//*[@id='oop']/option[28]")).click()
-
-
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.wait import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.support.ui import Select 
-import time 
- 
-
- 
+driver = webdriver.Chrome(options=options) 
 driver.get("https://priem.volsu.ru/rating") 
- 
+driver.maximize_window()
+
 webElement = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'level'))) 
 select = Select(webElement).select_by_value('  Бакалавриат') 
  
@@ -66,4 +35,19 @@ WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'form'))).sen
 
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'base'))).send_keys('бюджетная основа') 
 
-searchBtn = Select(driver.find_element(by=By.ID, value='search').click())
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'search'))).click() 
+
+table = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#rating_table'))) 
+
+items = table.find_elements(By.XPATH, "//*[@class='yellow' or @class='default' or @class='in-order-first']")
+
+from selenium.webdriver.common.action_chains import ActionChains 
+action = ActionChains(driver) 
+for item in items: 
+    action.move_to_element(item).perform() 
+    table = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.detail.found'))) 
+    with open('filename.txt', 'a', encoding="utf-8") as f:
+        f.write(table.get_attribute('innerHTML'))
+        f.write('\n\n')
+    print(table.get_attribute('innerHTML')) 
+
